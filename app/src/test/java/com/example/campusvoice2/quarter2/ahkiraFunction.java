e7428c9dd6dfbe2b15410e3e7c2b1475d4a505b9
SHS PC <ahkiramaniclang12@gmail.com>
2026-09-02 15:09:40 +0800

    Ahkira Mini PETA 1

---
+++ app/src/test/java/com/example/campusvoice2/quarter2/ahkiraFunction.java
@@ -0,0 +1,29 @@
+package com.example.campusvoice2.quarter2;
+
+import org.junit.Test;
+
+public class ahkiraFunction {
+    @Test
+    public void printMyProfile() {
+
+// --1. THE INPUT (Storing your personal details in variables)
+
+        String myName = "Maria";
+
+        String petName = "Bantay";
+
+        String favFood = "Sinigang";
+
+        int myAge = 16;
+
+// --- 2. THE OUTPUT (Printing to the console)
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+
+        System.out.println("I have a wonderful pet named " + petName + ".");
+
+        System.out.println("If I could, I would eat " + favFood + " every single day!");
+    }
+}
