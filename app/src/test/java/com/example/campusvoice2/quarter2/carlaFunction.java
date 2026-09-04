fcb29b2d38fa38fefe4a1103b5af96294ce29cb3
Carla Rodriguez <cmpr063010@gmail.com>
2026-09-05 01:41:47 +0800

    FINAL MINI PETA 1 - CARLA

---
+++ app/src/test/java/com/example/campusvoice2/quarter2/carlaFunction.java
@@ -0,0 +1,21 @@
+package com.example.campusvoice2.quarter2;
+
+import org.junit.Test;
+
+public class carlaFunction {
+    @Test
+    public void printMyProfile() {
+     // --1. THE INPUT (Storing your personal details in variables)
+        String myName = "Carla";
+        String petName = "Twiksi";
+        String favFood = "Porkchop";
+        int myAge = 16;
+
+    // --- 2. THE OUTPUT (Printing to the console)
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day!");
+        }
+}
+
